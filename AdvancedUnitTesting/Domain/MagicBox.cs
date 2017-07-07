b449f8904f606fa34dec600481de62f44859ee58
timo.toivonen <timo.toivonen@gfs-web.de>
2017-07-07 09:03:47 +0200

    added fluent assertion example for event monitoring

---
+++ AdvancedUnitTesting/Domain/MagicBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AdvancedUnitTesting.Domain
+{
+    public class MagicBox
+    {
+        public string MyMethod(int a)
+        {
+            if (a == 3)
+            {
+                ErgebnisseReady?.Invoke(this, new MyEventArgs("Hello"));
+                return null;
+            }
+            else
+                return "Thanks";
+        }
+
+        public event EventHandler<MyEventArgs> ErgebnisseReady;
+
+        public class MyEventArgs : EventArgs
+        {
+            public MyEventArgs(string msg) : base() { this.Message = msg; }
+            public string Message { get; private set; }
+        }
+    }
+}
---
+++ AdvancedUnitTesting/FluentAssertionsEventMonitoringTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using AdvancedUnitTesting.Domain;
+
+namespace AdvancedUnitTesting
+{
+    [TestClass]
+    public class FluentAssertionsEventMonitoringTest
+    {
+        [TestMethod]
+        public void WhenMyMethodCalled_ThenThanksIsReturned()
+        {
+            var empty = new MagicBox();
+            empty.MyMethod(1).Should().Be("Thanks");
+        }
+
+        [TestMethod]
+        public void WhenMyMethodParamIs3_ThenEventIsRaisedAndThanksReturned()
+        {
+            var empty = new MagicBox();
+            empty.MonitorEvents();
+            empty.MyMethod(3).Should().BeNull();
+            empty.ShouldRaise("ErgebnisseReady");
+        }
+    }
+}
